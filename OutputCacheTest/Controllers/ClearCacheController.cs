f6ef82d72bd58207d1a1581805b1cf1b13a2b1d5
taherehfarrokhi <taherehfarrokhi@TAHEREHFARRFB32>
2016-08-21 23:10:38 +0100

    Add project files.

---
+++ OutputCacheTest/Controllers/ClearCacheController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Web.Mvc;
+
+namespace OutputCacheTest.Controllers
+{
+    public class ClearCacheController : Controller
+    {
+        // GET: ClearCache
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ClearCache()
+        {
+            ClearCacheData();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private void ClearCacheData()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var result = httpClient.GetStringAsync("http://localhost:52163/?f=").Result;
+            }
+        }
+    }
+}
---
+++ OutputCacheTest/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace OutputCacheTest.Controllers
+{
+    [OutputCache(Duration = int.MaxValue, Location = OutputCacheLocation.Server, VaryByParam = "None")]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Response.Cache.AddValidationCallback(ValidateCache, null);
+            return View(DateTime.Now);
+        }
+
+        private void ValidateCache(HttpContext context, object data, ref HttpValidationStatus validationstatus)
+        {
+            if(context.Request.QueryString.AllKeys.Contains("f"))
+                validationstatus = HttpValidationStatus.Invalid;
+        }
+    }
+}
